2f3e82e03a461fb1079e7ede3274af671f43ee73
Aprianto Wiwarsono <awiwarsono@gmail.com>
2017-10-13 06:39:18 +1300

    Passed the test for tree but not optimal

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  newTree.children = [];
 
-  // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,13 +11,27 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  let node = Tree(value);
+  this.children.push(node);
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  } else if (this.children.length > 0) {
+    return _.reduce(this.children, function (result, node) {
+      if (result) {
+        return true;
+      }
+      return node.contains(target);
+    }, false);
+  } else {
+    return false;
+  }
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addChild: constant
+ * contains:
  */
