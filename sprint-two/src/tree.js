a90b041d1c46cbce82052839100a5735c73484d2
Aprianto Wiwarsono <awiwarsono@gmail.com>
2017-10-14 07:24:23 +1300

    Update time complexity for Graph and Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -33,6 +33,6 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  * addChild: constant
- * contains: log n
- * Total: log n
+ * contains: linear
+ * Total: linear
  */
