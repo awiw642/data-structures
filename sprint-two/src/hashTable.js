371d1de924ee93ce5585d64a22df268c62de440e
Aprianto Wiwarsono <awiwarsono@gmail.com>
2017-10-14 07:24:52 +1300

    Completed Hash Table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,3 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,20 +5,56 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let result = [];
+  let newValue = [k, v];
+  let valuesAtIndex = this._storage.get(index);
+
+  if (valuesAtIndex !== undefined) {
+    valuesAtIndex.forEach((currentValue) => {
+      if (currentValue[0] !== k) {
+        result.push(currentValue);
+      }
+    });
+  }
+  result.push(newValue);
+
+  this._storage.set(index, result);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let valuesAtIndex = this._storage.get(index);
+  let result;
+
+  if (valuesAtIndex !== undefined) {
+    for (let i = 0; i < valuesAtIndex.length; i++) {
+      if (valuesAtIndex[i][0] === k) {
+        result = valuesAtIndex[i][1];
+        break;
+      }
+    }
+  }
+  return result;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let valuesAtIndex = this._storage.get(index);
+  let result = [];
+
+  if (valuesAtIndex !== undefined) {
+    for (let i = 0; i < valuesAtIndex.length; i++) {
+      if (valuesAtIndex[i][0] !== k) {
+        result.push(valuesAtIndex[i]);
+      }
+    }
+  }
+  this._storage.set(index, result);
+  return k;
 };
-
-
-
 /*
  * Complexity: What is the time complexity of the above functions?
+ * insert: typically constant, worst case linear
+ * retrieve: typically constant, worst case linear
+ * remove: typically constant, worst case linear
  */
-
-
